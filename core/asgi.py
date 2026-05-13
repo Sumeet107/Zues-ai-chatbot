bd1e7ef3bc106c0299ef567966ce8b2926cbe790
Sumeet107 <sumeetvaja81@gmail.com>
2026-05-13 13:40:54 +0530

    Upload: core/asgi.py

---
+++ core/asgi.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from chatbot.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+
+    "http": get_asgi_application(),
+
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
